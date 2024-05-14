f85e1bf4b4c5e29e577ac8cfe91e97e871339718
gowthamnatarajan03 <gowthamnatarajan03@gmail.com>
2024-05-14 16:12:00 +0530

    Second Commit, Environment Variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    googleMapsApiKey: 'process.env.GOOGLE_MAPS_API_KEY'
+    googleMapsApiKey: ''
   };
   
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    googleMapsApiKey: 'process.env.GOOGLE_MAPS_API_KEY'
+    googleMapsApiKey: ''
   };
   
